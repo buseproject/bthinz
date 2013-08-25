547b24dd0dce91213ff669866ab0083894a02756
Prabhakaran <prabha.7g@gmail.com>
2013-08-25 06:44:14 +0530

    Initial files added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bthinz::Application.config.secret_key_base = 'fa47ec4c87a07ff7059a26d781fc8d95fe3124bd00913e656eea9e88f7e28325a631a0bbc7a35079926d572d606a962bba5ddc3915e7571d67d28aea3b7a3538'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bthinz::Application.config.session_store :cookie_store, key: '_bthinz_session'
